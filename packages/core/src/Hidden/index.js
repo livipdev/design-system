a3a71b0105c663c1bd21452414abdf1b6eef52d5
Matheus Rodrigues <matheusrod92@gmail.com>
2020-05-23 15:37:57 -0300

    :construction: Add Hidden component

---
+++ packages/core/src/Hidden/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import MuiHidden from '@material-ui/core/Hidden';
+
+const Hidden = (props) => <MuiHidden {...props} />;
+
+Hidden.propTypes = MuiHidden.propTypes;
+
+export default Hidden;
